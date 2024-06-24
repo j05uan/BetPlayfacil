fea3d403495885f8db8902ad1334993e071d11a9
j05uan <lopezamayajuanpablo@gmail.com>
2024-06-24 16:32:24 -0500

    1

---
+++ src/main/java/com/soccer/Controller.java
@@ -0,0 +1,10 @@
+package com.soccer;
+
+
+import java.util.Hashtable;
+
+import com.soccer.model.entity.Team;
+
+public class Controller {
+    public Hashtable <String,Team> equipos = new Hashtable<>();
+}
